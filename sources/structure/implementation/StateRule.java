a5f2bccb7931e66e5d31c197956a8af3a8983359
Sami Saada <saada@cs.helsinki.fi>
2011-09-12 01:19:33 +0300

    removed legacy stuff

--- sources/structure/implementation/StateRule.java
+++
@@ -1,8 +0,0 @@
-package structure.implementation;
-
-public interface StateRule
-{
-    public boolean match(char c);
-    public boolean equals(Object rule);
-    public String toString();
-}
--- sources/structure/implementation/TreeData.java
+++
@@ -1,5 +0,0 @@
-package structure.implementation;
-
-public interface TreeData
-{
-}
