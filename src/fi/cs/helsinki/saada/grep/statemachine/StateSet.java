5e669d6f64a3f619fa09340af4008c1ead2205c3
Sami Saada <saada@cs.helsinki.fi>
2011-10-16 02:32:44 +0300

    made StateSet really add and delete states

--- src/fi/cs/helsinki/saada/grep/statemachine/StateSet.java
+++ src/fi/cs/helsinki/saada/grep/statemachine/StateSet.java
@@ -3,23 +3,69 @@ package fi.cs.helsinki.saada.grep.statemachine;
 public class StateSet {
 
     private int count;
+    private State[] states;
 
     public StateSet() {
         this.count = 0;
+        this.states = new State[1];
     }
 
     public int count() {
         return this.count;
     }
 
-    public void add(State state) {
-        this.count++;
+    public void add(State state) throws Exception {
+        if (this.includes(state))
+            throw new Exception("state already found");
+        this.insert(state);
+    }
+
+    public boolean includes(State state) {
+        return (this.index(state) != -1);
     }
 
     public void delete(State state) throws Exception {
-        if (this.count() == 0)
-            throw new Exception("no more states");
+        if (!this.includes(state))
+            throw new Exception("state not found");
+        this.remove(state);
+    }
+
+    private void insert(State state) {
+        this.states[this.count()] = state;
+        this.count++;
+        this.checkStates();
+    }
+
+    private void remove(State state) {
+        int removed_index = this.index(state);
+        for(int i=removed_index; i < this.count(); i++) {
+            this.states[i] = this.states[i+1];
+        }
         this.count--;
+        this.checkStates();
+    }
+
+    private int index(State state) {
+        for(int i=0; i < this.count(); i++) {
+            if (this.states[i] == state)
+                return i;
+        }
+        return -1;
+    }
+
+    private void checkStates() {
+        if (this.count() == this.states.length)
+            this.alterStates(this.states.length*2);
+        else if (this.count() < this.states.length/2-1)
+            this.alterStates(this.states.length/2);
+    }
+
+    private void alterStates(int size) {
+        State[] newStates = new State[size];
+        for(int i=0; i < this.count(); i++) {
+            newStates[i] = this.states[i];
+        }
+        this.states = newStates;
     }
 
 }
--- test/fi/cs/helsinki/saada/grep/statemachine/StateSetTest.java
+++ test/fi/cs/helsinki/saada/grep/statemachine/StateSetTest.java
@@ -32,11 +32,46 @@ public class StateSetTest {
             this.set = new StateSet();
         }
 
-        public void testItShouldAddCount() {
+        public void testItShouldAddCount() throws Exception {
             this.set.add(this.state1);
             assertEquals(1, this.set.count());
             this.set.add(this.state2);
             assertEquals(2, this.set.count());
+        }
+
+        public void testItShouldNotAddAlreadyAddedState() throws Exception {
+            this.set.add(this.state1);
+            assertEquals(1, this.set.count());
+            try {
+                this.set.add(this.state1);
+                fail("Should throw exception");
+            } catch(Exception e) {
+            }
+            assertEquals(1, this.set.count());
+        }
+
+    }
+
+    public static class Including extends TestCase {
+
+        private State state1, state2;
+        private StateSet set;
+
+        public void setUp() {
+            this.state1 = createMock(State.class);
+            this.state2 = createMock(State.class);
+            this.set = new StateSet();
+        }
+
+        public void testItShouldIncludeAddedState() throws Exception {
+            this.set.add(this.state1);
+            assertTrue(this.set.includes(this.state1));
+        }
+
+        public void testItShouldNotIncludeNotAddedState() throws Exception {
+            assertFalse(this.set.includes(this.state1));
+            this.set.add(this.state1);
+            assertFalse(this.set.includes(this.state2));
         }
 
     }
@@ -56,20 +91,29 @@ public class StateSetTest {
             this.set.add(this.state1);
             this.set.add(this.state2);
             assertEquals(2, this.set.count());
-            this.set.delete(this.state1);
+            try {
+                this.set.delete(this.state1);
+            } catch(Exception e) {
+                fail("First state deleting failed");
+            }
             assertEquals(1, this.set.count());
-            this.set.delete(this.state2);
+            try {
+                this.set.delete(this.state2);
+            } catch(Exception e) {
+                fail("Second state deleting failed");
+            }
             assertEquals(0, this.set.count());
         }
 
-        public void testItShouldRaiseErrorWhenDeletingNonExistState() {
-            assertEquals(0, this.set.count());
+        public void testItShouldRaiseErrorWhenDeletingNonExistState() throws Exception {
+            this.set.add(this.state1);
+            assertEquals(1, this.set.count());
             try {
-                this.set.delete(this.state1);
+                this.set.delete(this.state2);
                 fail("Should throw exception");
             } catch(Exception e) {
             }
-            assertEquals(0, this.set.count());
+            assertEquals(1, this.set.count());
         }
 
     }
