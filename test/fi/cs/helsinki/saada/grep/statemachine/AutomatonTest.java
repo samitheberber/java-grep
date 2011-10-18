e93d4d33a89d75499557305769f49c9a18fbf378
Sami Saada <saada@cs.helsinki.fi>
2011-10-19 02:20:37 +0300

    added rescue system for unmet state

--- test/fi/cs/helsinki/saada/grep/statemachine/AutomatonTest.java
+++ test/fi/cs/helsinki/saada/grep/statemachine/AutomatonTest.java
@@ -84,26 +84,34 @@ public class AutomatonTest {
 
         private class Automaton {
 
+            private StateSet states;
             private Delta delta;
             private State start;
             private StateSet goals;
 
             public Automaton(StateSet states, Vocabulary vocabulary, Delta delta, State start, StateSet goals) {
+                this.states = states;
                 this.delta = delta;
                 this.start = start;
                 this.goals = goals;
             }
 
-            public boolean run(String characters) {
+            public boolean run(String characters) throws Exception {
                 return this.goals.includes(this.result(characters));
             }
 
-            private State result(String characters) {
+            private State result(String characters) throws Exception {
                 State current = this.start;
                 for(char c : characters.toCharArray()) {
                     current = this.delta.calculate(current, c);
+                    this.ensure(current);
                 }
                 return current;
+            }
+
+            private void ensure(State state) throws Exception {
+                if (!this.states.includes(state))
+                    throw new Exception("Undeclared state met");
             }
 
         }
@@ -142,21 +150,21 @@ public class AutomatonTest {
             assertNotNull(this.automate);
         }
 
-        public void test__first_state_accepts_and_automate_runs_successful_with_empty_string() {
+        public void test__first_state_accepts_and_automate_runs_successful_with_empty_string() throws Exception {
             expect(this.F.includes(this.q0)).andReturn(true);
             replay(this.F);
             assertTrue(this.automate.run(""));
             verify(this.F);
         }
 
-        public void test__first_state_rejects_and_automate_runs_unsuccessful_with_empty_string() {
+        public void test__first_state_rejects_and_automate_runs_unsuccessful_with_empty_string() throws Exception {
             expect(this.F.includes(this.q0)).andReturn(false);
             replay(this.F);
             assertFalse(this.automate.run(""));
             verify(this.F);
         }
 
-        public void test__further_state_accepts_and_automate_runs_successful_with_string_that_belongs_to_the_language() {
+        public void test__further_state_accepts_and_automate_runs_successful_with_string_that_belongs_to_the_language() throws Exception {
             expect(this.delta.calculate(this.q0, 'f')).andReturn(this.q0);
             expect(this.delta.calculate(this.q0, 'o')).andReturn(this.q1);
             expect(this.delta.calculate(this.q1, 'o')).andReturn(this.q3);
@@ -164,25 +172,57 @@ public class AutomatonTest {
             expect(this.delta.calculate(this.q1, 'a')).andReturn(this.q0);
             expect(this.delta.calculate(this.q0, 'r')).andReturn(this.q2);
             replay(this.delta);
+            expect(this.Q.includes(this.q0)).andReturn(true);
+            expect(this.Q.includes(this.q0)).andReturn(true);
+            expect(this.Q.includes(this.q1)).andReturn(true);
+            expect(this.Q.includes(this.q1)).andReturn(true);
+            expect(this.Q.includes(this.q2)).andReturn(true);
+            expect(this.Q.includes(this.q3)).andReturn(true);
+            replay(this.Q);
             expect(this.F.includes(this.q2)).andReturn(true);
             replay(this.F);
             assertTrue(this.automate.run("foobar"));
             verify(this.delta);
+            verify(this.Q);
             verify(this.F);
         }
 
-        public void test__further_state_rejects_and_automate_runs_unsuccessful_with_string_that_doesnt_belong_to_the_language() {
+        public void test__further_state_rejects_and_automate_runs_unsuccessful_with_string_that_doesnt_belong_to_the_language() throws Exception {
             expect(this.delta.calculate(this.q0, 'f')).andReturn(this.q0);
             expect(this.delta.calculate(this.q0, 'o')).andReturn(this.q1);
             expect(this.delta.calculate(this.q1, 'b')).andReturn(this.q3);
             expect(this.delta.calculate(this.q3, 'a')).andReturn(this.q2);
             expect(this.delta.calculate(this.q2, 'r')).andReturn(this.q3);
             replay(this.delta);
+            expect(this.Q.includes(this.q0)).andReturn(true);
+            expect(this.Q.includes(this.q1)).andReturn(true);
+            expect(this.Q.includes(this.q2)).andReturn(true);
+            expect(this.Q.includes(this.q3)).andReturn(true);
+            expect(this.Q.includes(this.q3)).andReturn(true);
+            replay(this.Q);
             expect(this.F.includes(this.q3)).andReturn(false);
             replay(this.F);
             assertFalse(this.automate.run("fobar"));
             verify(this.delta);
+            verify(this.Q);
             verify(this.F);
+        }
+
+        public void test__met_not_belonging_state_so_panic() {
+            State q4 = createMock(State.class);
+            expect(this.delta.calculate(this.q0, 'f')).andReturn(this.q0);
+            expect(this.Q.includes(this.q0)).andReturn(true);
+            expect(this.delta.calculate(this.q0, 'u')).andReturn(q4);
+            expect(this.Q.includes(q4)).andReturn(false);
+            replay(this.delta);
+            replay(this.Q);
+            try {
+                this.automate.run("fubar");
+                fail("Exception must be raised");
+            } catch(Exception e) {
+                verify(this.delta);
+                verify(this.Q);
+            }
         }
 
     }
