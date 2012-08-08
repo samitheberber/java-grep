d74bb05b0fd0c5644e0a0e6f86ebc1c29f4eb88a
Sami Saada <saada@cs.helsinki.fi>
2012-08-08 14:18:08 +0300

    state tests contain only relevant tests

--- src/fi/cs/helsinki/saada/grep/statemachine/State.java
+++ src/fi/cs/helsinki/saada/grep/statemachine/State.java
@@ -2,35 +2,17 @@ package fi.cs.helsinki.saada.grep.statemachine;
 
 public class State implements DeltaResult {
 
-    private boolean accepting;
     private String name;
 
     public State() {
-        this(false);
-    }
-
-    public State(boolean accepting) {
-        this(accepting, "");
+        this("");
     }
 
     public State(String name) {
-        this(false, name);
-    }
-
-    public State(boolean accepting, String name) {
-        this.setAcceptance(accepting);
         this.name = name;
     }
 
-    public boolean isAccepting() {
-        return this.accepting;
-    }
-
-    public void setAcceptance(boolean accepting) {
-        this.accepting = accepting;
-    }
-
-    public String getName() {
+    public String toString() {
         return this.name;
     }
 
